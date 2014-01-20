98cbd2c32c55af9cb8f674c918c0ba36b4ff9b69
Phil Lavin <phil@propcom.co.uk>
2014-01-20 16:42:51 +0000

    Implement an admin menu

--- raspi-fprint-relay.c
+++ raspi-fprint-relay.c
@@ -9,6 +9,10 @@
 // Global vars
 state system_state = ENROLLMENT;
 int lcd_state;
+int admin_line;
+
+#define LCD_STATE_DEFAULT 0
+#define LCD_STATE_ADMIN 1
 
 int main(void) {
 	// Vars
@@ -62,16 +66,52 @@ void lcd_reset() {
 	clear_screen();
 	write_string("How Many Cans? 1");
 	cursor_left(1);
+	cursor_on();
 	cursor_blink_on();
 
-	lcd_state = 0;
+	lcd_state = LCD_STATE_DEFAULT;
+}
+
+void write_admin_menu(int shift) {
+	int i;
+
+	// The options available
+	const char *options[] = {
+		"1. Restock",
+		"2. Enroll",
+		"3. Deal Drugs"
+	};
+	const int num_options = 3; // Should be the number of things in the above
+
+	// Don't let us shift more than the number of options
+	if (shift > num_options) {
+		return;
+	}
+
+	// Whealer's fault. Shift options around in a circle
+	admin_line += shift + num_options;
+	admin_line %= num_options;
+
+	// Clean up
+	clear_screen();
+	cursor_blink_off();
+	cursor_off();
+
+	// Write the first thing
+	write_string(options[admin_line]);
+
+	// Write the next thing - go around in a circle. Also Whealer's fault
+	write_string("\n");
+	write_string(options[(admin_line + 1) % num_options]);
 }
 
 void handle_key_press(char key) {
 	char *tri_code;
 
+	// Standard key presses...
+
 	// Default "How Many Cans?" screen
-	if (lcd_state == 0) {
+	if (lcd_state == LCD_STATE_DEFAULT) {
 		// 1 - 9
 		if (key >= 49 && key <= 57) {
 			write_char(key);
@@ -79,12 +119,51 @@ void handle_key_press(char key) {
 		}
 	}
 
-	if (tri_code = (char *)get_tri_code()) {
-		if (strcmp(tri_code, "*69") == 0) {
-			clear_screen();
-			write_string("Admin MOFO!\nKeith Wheal");
+	// Admin menu
+	if (lcd_state == LCD_STATE_ADMIN) {
+		if (key == '#') {
+			write_admin_menu(1);
+		}
+		else if (key == '*') {
+			write_admin_menu(-1);
+		}
+		// 1 - 9
+		if (key >= 49 && key <= 57) {
+			switch (key) {
+				// Restock
+				case '1':
+					// Unlock teh door
+					break;
+				// Enroll
+				case '2':
+					// Enroll
+					break;
+				// Drug deal
+				case '3':
+					// Deduct from user 'drugs'
+					// Open door
+					break;
+			};
 		}
 	}
 
-	printf("Key press of %c\n", key);
+	// Tri codes...
+
+	// *69 for admin
+	if (tri_code = (char *)get_tri_code()) {
+		if (strcmp(tri_code, "*69") == 0) {
+			clear_screen();
+			cursor_blink_off();
+			cursor_off();
+			write_string("Admin mode...\n* & # to scroll");
+
+			admin_line = 0;
+
+			sleep(2);
+
+			write_admin_menu(0);
+
+			lcd_state = LCD_STATE_ADMIN;
+		}
+	}
 }
